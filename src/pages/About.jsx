3e1074b5bfa7680e3f5dc02aadfcc8776953ff59
ekremdermanel <ekremdermanel@gmail.com>
2022-01-20 16:34:38 +0300

    project start

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+function About() {
+    return (
+        <div>
+            <h1 className="text-6xl mb-4">Github Finder</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, eum minus provident ipsum autem libero quas velit praesentium temporibus. Sed.
+            </p>
+        </div>
+    )
+}
+
+export default About
